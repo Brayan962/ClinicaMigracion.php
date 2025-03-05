d3d94bf0be8c774cad5ef693491875cb063808d2
Brayan <usuario@DESKTOP-8M2PEVT>
2025-03-05 16:39:25 -0600

    brayanClinica/Migraciones

---
+++ app/Http/Controllers/ParamedicosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\paramedicos;
+use Illuminate\Http\Request;
+
+class ParamedicosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(paramedicos $paramedicos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(paramedicos $paramedicos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, paramedicos $paramedicos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(paramedicos $paramedicos)
+    {
+        //
+    }
+}
